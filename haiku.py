7d1be3e672321a4e038932873fb220ff9b87f646
Radovan Bast <bast@users.noreply.github.com>
2016-12-07 17:22:33 +0100

    Haiku changes on new_feature.

---
+++ haiku.py
@@ -0,0 +1,9 @@
+def get_haiku():
+#modify something on haiku
+#modify more on haiku
+    return '''On a branch ...
+                  by Kobayashi Issa
+
+              On a branch
+              floating downriver
+              a cricket, singing.'''
